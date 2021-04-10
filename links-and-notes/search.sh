30780b1271fea01b5f0fc7b8159235f2b83d18cf
Karl Xu <karl.yogi.xu@gmail.com>
2021-04-10 22:38:45 +0800

    links-and-notes: optimize search tool

--- links-and-notes/search.sh
+++ links-and-notes/search.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
 
-if [ -z "${1}" ] ; then 
-    echo "Search pattern can not be empty."
-    exit 1
-fi
+function usage {
+  echo "Help information:
+--content Search by links and notes content
+  "
+}
 
-git grep -i --name-only $1
+# if [ -z "${1}" ] ; then 
+#     echo "Search pattern can not be empty."
+#     exit 1
+# fi
+
+while [[ $# -gt 0 ]]
+do 
+key="$1"
+case $key in
+    --content)
+    CONTENT="$2"
+    shift
+    shift 
+    ;;
+
+    -h)
+    usage
+    exit 0
+    ;;
+
+    *)
+    echo "Unknow parameter."
+    usage
+    exit 1
+    ;;
+esac
+done
+
+git grep -i --name-only ${CONTENT}
