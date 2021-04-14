2464e41138a5432c3362388c13fa06e1e952f9fc
Karl Xu <karl.yogi.xu@gmail.com>
2021-04-14 18:26:44 +0800

    optimize links and notes helping tool

---
+++ links-and-notes/helper.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+function usage {
+  echo "Help information:
+
+    --search Search by links and notes content
+
+    --edit   Edit the target link and note
+  "
+}
+
+if [ $# -le 1 ]; then
+    echo "\nNot enough parameters passed.\n"
+    usage
+    exit 1
+fi
+
+while [ $# -gt 0 ]
+do 
+key="$1"
+case $key in
+
+    # when meet --search option break out of the loop
+    --search)
+      git grep -i --name-only "${2}"
+    break
+    ;;
+
+    --edit)
+      vim "${2}"
+    break
+    ;;
+
+    -h)
+    usage
+    exit 0
+    ;;
+
+    *)
+    echo "Unknow parameter."
+    usage
+    exit 1
+    ;;
+esac
+done
+
